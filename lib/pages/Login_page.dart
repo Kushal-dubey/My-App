b9a890777c79550a37677c8c3c63e89e0852fc07
Kushal Dubey <kushal13dubey@gmail.com>
2024-03-07 02:16:53 +0530

    Day-7 completed

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -1,3 +1,4 @@
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter/widgets.dart";
 import "package:flutter_application_1/utils/Routes.dart";
@@ -13,121 +14,155 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = " ";
+  bool changeButton = false;
+  final _formKey = GlobalKey<
+      FormState>(); // form key genarally not sending blank data on next page
+
+      // method calling the blank not going to not page
+      moveToHome(BuildContext context)async{
+        if(_formKey.currentState!= null ){     //methode to invoke and validate that empty buttons cannot be redirected to the next page
+          if(_formKey.currentState!.validate()){
+        setState(() {
+    //command set state with on tap widget
+     changeButton = true;
+      });
+  await Future.delayed(Duration(seconds: 1)); // after 1 seconds of clickig it will redirect us on the next page
+                          // await navigation is been usedyo agian fix the button after animation into its original shapw
+  await Navigator.pushNamed(context,MyRoutes.homeRoute,);
+  setState(() {
+                            // this set state is been used to again fix the button after animation and also get back into its original shape
+                            //command set state with on tap widget
+  changeButton = false;
+      });
+        }
+        }
+      }       //method to click only data is filled no the blank data is used
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
         // it is to used to make the scroll view of different phones
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/Login_image.png", //location in the column children and the image.assets
-              fit: BoxFit.cover,
-              height: 400,
+        child: Form(
+          //form is the parent of textformfields for usernme as well as for password
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/hey.png", //location in the column children and the image.assets
+                fit: BoxFit.cover,
+                height: 400,
 
-              // it is the property basically to fit the image
-            ),
-
-            // it is used to make the perfect size between image and welcome
-
-            Text(
-              "Welcome $name", //name is been added for the stateful widget
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+                // it is the property basically to fit the image
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
 
-            //padding is used in it to wrap the column elements and make username and password
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                  vertical: 16.0,
-                  horizontal: 32.0), //this is the symmetric padding
-              child: Column(
-                children: [
-                  TextFormField(
-                    //used to form the buttons like username and password
-                    decoration: InputDecoration(
-                        hintText: "Enter Username", labelText: "Username"),
+              // it is used to make the perfect size between image and welcome
 
-                    // this is the concept of stateful widget not stateless;
+              Text(
+                "Welcome $name", //name is been added for the stateful widget
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
 
-                    onChanged: (value) {
-                      // it is been used to genrally add something in username as well with welcome
-                      name = value;
-                      setState(() {
-                        //used to rebuild the ui in the case of stateful widget as well an also applied in case of stateful widget only.
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true, //used to secure and hode the password
-                    decoration: InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
-                  ),
+              //padding is used in it to wrap the column elements and make username and password
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 32.0), //this is the symmetric padding
+                child: Column(
+                  children: [
+                    TextFormField(
+                      //used to form the buttons like username and password
+                      decoration: InputDecoration(
+                          hintText: "Enter Username", labelText: "Username"),
 
-                  //buttons
-                  SizedBox(
-                    height: 40.0,
-                  ),
+                      // this is the concept of stateful widget not stateless;
+                    validator: (value) {
+                  if (value == null || value.isEmpty) {
+                   return "Username cannot be empty";
+                  }
+                   return null;
+                  },
 
-                  ElevatedButton(
-                    onPressed: () async {
-                      await Future.delayed(Duration(
-                          seconds:
-                              0)); // future delay how much tym it will redirect to homepage from loginpage
-                      Navigator.pushNamed(
-                        context,
-                        MyRoutes.homeRoute,
-                      );
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontStyle: FontStyle.normal,
-                        color: Colors.white,
+                      onChanged: (value) {
+                        //valodation only in textform field
+                        // it is been used to genrally add something in username as well with welcome
+                        name = value;
+                        setState(() {
+                          //used to rebuild the ui in the case of stateful widget as well an also applied in case of stateful widget only.
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true, //used to secure and hide the password
+                      decoration: InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                         validator: (value) {
+                if (value == null || value.isEmpty) {
+                return "Password cannot be empty";
+                   }
+               else if (value.length<6) {
+                return "Password length should be atleast 6";
+                   }
+                  },
+
+                    ),
+
+                    //buttons
+                    SizedBox(
+                      height: 40.0,
+                    ),
+
+                    // Elevated  Button for Login in an Animated Way
+                    //Inkwell is been used for animation factors
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius: //border for onclicking the button
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          //changebutton? is the condition either true or false in case of animation
+                          height: 50,
+                          alignment: Alignment.center,
+                          child:
+                              changeButton //this is been used as an vbool function in stateful widget
+                                  ? Icon(
+                                      Icons.done,
+                                      color: Colors.white,
+                                    ) //animation of done after clicking on login
+                                  : Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                          //decoration: BoxDecoration(   // decoration beyod the button
+                          // shape:
+                          // changeButton ? BoxShape.circle : BoxShape.rectangle,
+                          //borderRadius:
+                          //  BorderRadius.circular(changeButton ? 50 : 8),
+                          //color: Colors.deepPurple,
+                        ),
                       ),
                     ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 13, 54, 87),
-                      minimumSize: Size(150, 60),
-                    ),
-                  ),
+                    //)
 
-// Elevated Button for login
-                  /*  ElevatedButton(
-                    onPressed: Async() {
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(
-                          context,
-                          MyRoutes
-                              .homeRoute); //navigator is used to navigate through this myroute concpt and it is the static one so no need to change again and again only once modified.
-                    },
-                    //onpressed is the function in which button is been operated
-// Button designed for login
-
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontStyle: FontStyle.normal,
-                        color: Colors.white,
-                      ),
-                    ),
-
-                    style: TextButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 13, 54, 87),
-                        minimumSize: Size(150, 60)),
-                  ), */ //used to make button
-                ],
+                    //
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       // this is the new widget called image assets in the children column
@@ -139,25 +174,7 @@ class _LoginPageState extends State<LoginPage> {
 
 
 // this is been used as container to make the login button attractive with animation on it.
- /*  InkWell(
-                    //ink well is used to click on the button with the container in it with on tap command
-                    onTap: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                  ), //container is used as the animation of the login button
-                  Container(
-                    width: 150,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: Text("Login",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold)),
-                    decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius: BorderRadius.circular(10)),
-                  )*/
+
 
 
 
@@ -181,3 +198,30 @@ class _LoginPageState extends State<LoginPage> {
         ),
         backgroundColor: Colors.indigo,
       ),*/
+
+
+
+//elevated button for login
+                  /*    ElevatedButton(
+                    onPressed: () async {
+                      await Future.delayed(Duration(
+                          seconds:
+                              0)); // future delay how much tym it will redirect to homepage from loginpage
+                      Navigator.pushNamed(
+                        context,
+                        MyRoutes.homeRoute,
+                      );
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 13, 54, 87),
+                      minimumSize: Size(150, 60),
+                    ),
+                  ),*/
